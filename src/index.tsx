3dac06f43dcfc58f4970f7a39b1ebae7813c54dc
Immanuel Washington <code.immanuelqrw@gmail.com>
2020-07-12 23:46:57 -0400

    fix: Added Craco to repair CRA TypeScript aliases
    Create-React-App doesn't accept TypeScript aliases and requires using Craco to run
    
    Added Craco successfully; `craco start` runs application

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-import {BingoTaskViewer} from "layouts/BingoTaskViewer";
+import {BingoTaskViewer} from "@layouts/BingoTaskViewer";
 
 // core components
 
-import "assets/css/material-dashboard-react.css?v=1.8.0";
+import "@assets/css/material-dashboard-react.css?v=1.8.0";
 
 const hist = createBrowserHistory();
 
